d038bb3a3ee810850c8cea9a9b1937a588fbb131
Stephan Hagemann <stephan.hagemann@gmail.com>
2025-04-26 23:37:49 -0600

    Fix manifest #2?

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["LiquidKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kitura/HTMLEntities.git", from: "3.0.0")
+        .package(url: "https://github.com/Kitura/swift-html-entities.git", from: "3.0.0")
     ],
     targets: [
         .target(
